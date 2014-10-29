09a98660d8411ad10ff51eaf6bae37682b8b4d16
golddiamonds <rhall@berkeley.edu>
2014-10-29 13:00:18 -0700

    Initial Commit.

---
+++ DnD/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+
+namespace DnD
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Printout of Chuul.xml");
+			XmlTextReader reader = new XmlTextReader ("/Users/ryanhall/Projects/DnD/DnD/MonsterXML/Chuul.xml");
+
+			while (reader.Read()) 
+			{
+				switch (reader.NodeType) 
+				{
+				case XmlNodeType.Element: // The node is an element.
+					Console.Write("<" + reader.Name);
+					Console.WriteLine(">");
+					break;
+				case XmlNodeType.Text: //Display the text in each element.
+					Console.WriteLine (reader.Value);
+					break;
+				case XmlNodeType.EndElement: //Display the end of the element.
+					Console.Write("</" + reader.Name);
+					Console.WriteLine(">");
+					break;
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
